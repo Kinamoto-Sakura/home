140366f07ea1ce612e220cbfc6bf7c7022c1d79a
Sakura <69903799+kafuneri@users.noreply.github.com>
2024-10-08 20:51:46 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -23,7 +23,7 @@
         <!-- 站点备案 -->
         <span>
           &amp;
-          <i class="icon-MOE"><a v-if="siteIcp" href="https://icp.gov.moe/?keyword=20240729" target="_blank">
+          <span class="icon-MOE"/><a v-if="siteIcp" href="https://icp.gov.moe/?keyword=20240729" target="_blank">
           {{ siteIcp }}
           </a>
         </span>
